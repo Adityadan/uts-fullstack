c888ad09723a19a35cd6a513854b031a2e4596d4
unknown <danirama282@gmail.com>
2023-11-20 15:38:13 +0700

    fix bug while adding more paragraph

--- add-paragraf.php
+++ add-paragraf.php
@@ -1,24 +1,36 @@
 <?php
 include("koneksi.php");
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id_cerita = $_POST['id_cerita'];
     $isi_paragraf = $_POST['isi_paragraf'];
 
-    // Lakukan operasi INSERT pada tabel paragraf
-    $query_tambah_paragraf = "INSERT INTO paragraf (idusers, idcerita, isi_paragraf, tanggal_buat) VALUES ('" . $_SESSION['idusers'] . "', '$id_cerita', '$isi_paragraf', NOW())";
-    $result_tambah_paragraf = mysqli_query($conn, $query_tambah_paragraf);
+    // Gunakan pernyataan bersiap-siap untuk memasukkan data dengan aman
+    $query_tambah_paragraf = "INSERT INTO paragraf (idusers, idcerita, isi_paragraf, tanggal_buat) VALUES (?, ?, ?, NOW())";
+    
+    // Persiapkan pernyataan
+    $stmt = mysqli_prepare($conn, $query_tambah_paragraf);
+    
+    // Ikatan parameter
+    mysqli_stmt_bind_param($stmt, "iss", $_SESSION['idusers'], $id_cerita, $isi_paragraf);
+    
+    // Jalankan pernyataan
+    $result_tambah_paragraf = mysqli_stmt_execute($stmt);
 
-    // Periksa apakah operasi INSERT berhasil
+    // Periksa apakah eksekusi berhasil
     if ($result_tambah_paragraf) {
         // Redirect kembali ke halaman lihat-cerita
         header("Location: lihat-cerita.php?id=$id_cerita");
         exit();
     } else {
-        echo "Gagal menambahkan paragraf: " . mysqli_error($conn);
+        echo "Gagal menambahkan paragraf: " . mysqli_stmt_error($stmt);
     }
+
+    // Tutup pernyataan
+    mysqli_stmt_close($stmt);
 }
 
-// Tutup koneksi ke database
+// Tutup koneksi database
 mysqli_close($conn);
 ?>
